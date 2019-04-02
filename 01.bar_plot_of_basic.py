265c4dca0e969f5b3f64ebff9987adf699ffee78
jhh130910 <jhh130910@163.com>
2019-04-02 13:01:58 +0800

    py

---
+++ 01.bar_plot_of_basic.py
@@ -0,0 +1,12 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+data = [go.Bar(
+
+                x=['giraffes', 'orangutans', 'monkeys'],
+                            y=[20, 14, 23],
+
+                            # orientation = 'h'
+                                )]
+
+py.iplot(data, filename='basic-bar')
