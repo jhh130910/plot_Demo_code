507a35f6efef69935cebf8c7cb50da9a2dc58f71
jhh130910 <jhh130910@163.com>
2019-04-03 17:13:22 +0800

    py

---
+++ 15.scatter_with_color_dimension.py
@@ -0,0 +1,18 @@
+import plotly.graph_objs as go
+import plotly.plotly as py
+
+import numpy as np
+
+trace1 = go.Scatter(
+    y = np.random.randn(500),
+    mode='markers',
+    marker=dict(
+        size=16,
+        color = np.random.randn(500), #set color equal to a variable
+        colorscale='Viridis',
+        showscale=True
+    )
+)
+data = [trace1]
+
+py.iplot(data, filename='scatter-plot-with-colorscale')
